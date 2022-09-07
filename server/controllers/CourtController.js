6e3931ef899ad83e49d86bdb4b019fb2cf079a99
Abizrh <abizarah857@gmail.com>
2022-09-08 00:42:05 +0700

    feat: success get all data court

---
+++ server/controllers/CourtController.js
@@ -0,0 +1,134 @@
+const { Court, User } = require('../models')
+
+module.exports = class CourtController {
+
+    static async getAll (req, res, next){
+
+        try {
+
+            const court = await Court.findAll()
+
+            res.status(200).json({
+                message: 'success get data court',
+                court
+            })
+            
+        } catch (error) {
+
+            console.log(error)
+            
+        }
+    }
+
+    static async createCourt (req, res, next){
+
+        const { name, description, UserId, openHour, closeHour, location } = req.body
+
+        const point =  { type: 'POINT', coordinates: [location]}
+
+        try {
+
+            const court = await Court.create({ name, description, UserId, openHour, closeHour, location: point})
+
+            res.status(201).json({
+                message: 'success create data court',
+                court
+            })
+
+            
+        } catch (error) {
+
+            console.log(error)
+            
+        }
+        
+    }
+
+    static async updateCourt (req, res, next){
+
+        const { name, description, UserId, openHour, closeHour, location } = req.body
+
+        const { id } = req.params
+
+        try {
+
+            const findCourt = await Court.findByPk(id)
+
+            if(!findCourt) throw {name: 'Not Found'}
+
+            const court = await Court.update({ name, description, UserId, openHour, closeHour, location }, {where: {
+
+                id: findCourt.id
+
+            }})
+
+            res.status(200).json({
+                message: 'success update court',
+                court
+            })
+            
+        } catch (error) {
+
+            console.log(error)
+            
+        }
+        
+    }
+
+    static async getDetail (req, res, next){
+
+        const { id } = req.params
+
+        try {
+
+            const court = await Court.findByPk(id, {
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id', 'usename', 'email', 'role', 'phoneNumber', 'address', 'balance']
+                    }
+                ]
+            })
+            
+            res.status(200).json({
+                message : 'success get detail',
+                court
+            })
+
+        } catch (error) {
+
+            console.log(error)
+            
+        }
+        
+    }
+
+    static async deleteCourt (req, res, next){
+
+        const { id } = req.params
+
+        try {
+
+            const findCourt = await Court.findByPk(id)
+
+            if(!findCourt) throw {name: 'Not Found'}
+
+            await Court.destroy({
+                where: {
+                    id: findCourt.id
+                }
+            })
+
+            res.status(200).json({
+                message: 'success delete court'
+            })
+            
+        } catch (error) {
+
+            console.log(error)
+            
+        }
+        
+    }
+}
+
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const adminRouter = require('./adminRouter')
+const courtRouter = require('./courtRouter')
 const customerRouter = require('./customerRouter')
 
 router.use('/admin', adminRouter)
 router.use('/customer', customerRouter )
+
+router.use('/courts', courtRouter)
 
 module.exports = router
