32970cf26d82f7caa8860eff3c4231d6752da42f
Ahmadabd3004 <ahmadazis3004@gmail.com>
2022-09-10 18:51:50 +0700

    create order and schedule

--- server/routers/customerRouter.js
+++ server/routers/customerRouter.js
@@ -1,4 +1,7 @@
 const UserController = require("../controllers/UserController");
+const SchedulesControllers = require("../controllers/SchedulesControllers");
+const CourtDetailController = require("../controllers/CourtDetailController");
+const OrderScheduleController = require("../controllers/OrderScheduleController");
 
 const authentication = require("../middlewares/authentication");
 
@@ -6,8 +9,12 @@ const customerRouter = require("express").Router();
 
 customerRouter.post("/login", UserController.login);
 customerRouter.post("/register", UserController.register);
+customerRouter.get("/schedules", SchedulesControllers.getSchedules);
+customerRouter.post("/court/order", OrderScheduleController.OrderSchedule);
+customerRouter.get("/court/orderList", OrderScheduleController.getOrder);
+customerRouter.patch("/court/cancelOrder", OrderScheduleController.cancelOrder);
+customerRouter.get("/court/:id", CourtDetailController.getDetailCourt);
 
-customerRouter.use(authentication);
-
+// customerRouter.use(authentication);
 
 module.exports = customerRouter;
