8a194cf633fb8f87a82254adeb15a3a2f3913d33
Abizrh <abizarah857@gmail.com>
2022-09-11 15:35:14 +0700

    add testing and api-doc for register customer

--- server/__tests__/customer.test.js
+++ server/__tests__/customer.test.js
@@ -17,7 +17,7 @@ const {sequelize} = require('../models')
 const { queryInterface } = sequelize
 
 describe('POST /customer/register ', () => {
-    it('should be return success message', async() => {
+    it.skip('should be return success message', async() => {
         const body = {
             username: "test3",
             email: "test3@gmail.com",
@@ -37,3 +37,194 @@ describe('POST /customer/register ', () => {
         expect(response.body).toHaveProperty('message', expect.any(String))
     })
 })
+
+describe('POST /customer/register - missing email', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            password: "12345",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+describe('POST /customer/register - missing password', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            email: "test3@gmail.com",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+describe('POST /customer/register - invalid email', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            email: "test3gmail.com",
+            password: "12345",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+describe('POST /customer/register - invalid password', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            email: "test3@gmail.com",
+            password: "",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+describe('POST /customer/register - email already exist', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            email: "test3@gmail.com",
+            password: "12345",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+describe('POST /customer/register - invalid email format', () => {
+    it('should be return an object with error message', async() => {
+        const body = {
+            username: "test3",
+            email: "test3",
+            password: "12345",
+            role: 'customer',
+            phoneNumber: "0986556",
+            address: "jl. earth",
+            balance: 90000,
+            location: [-6.287204, 106.839076]
+        }
+
+        const response = await request(app)
+        .post('/customer/register')
+        .send(body)
+
+        expect(response.status).toBe(400)
+        expect(response.body).toBeInstanceOf(Object)
+        expect(response.body).toHaveProperty('message', expect.any(String))
+    })
+})
+
+// describe('POST /customer/login - ok', () => {
+//     it('should be return an object with success message', async() => {
+//         const body = {
+//             email: "abhi2@mail.com",
+//             password: "12345",
+//         }
+
+//         const response = await request(app)
+//         .post('/customer/login')
+//         .send(body)
+//         // console.log(body, '<<<<<<<<<<<<=========')
+//         this.current_user_token = response.body.access_token
+
+//         expect(response.status).toBe(200)
+//         expect(response.body).toBeInstanceOf(Object)
+//         expect(response.body).toHaveProperty('access_token', expect.any(String))
+//         expect(response.body).toHaveProperty('name', expect.any(String))
+//         expect(response.body).toHaveProperty('role', expect.any(String))
+//         expect(response.body).toHaveProperty('email', expect.any(String))
+//     })
+// })
+
+// describe('POST /customer/login - invalid password', () => {
+//     it('should be return an object with error message', async() => {
+//         const body = {
+//             email: "abhi2@gmail.com",
+//             password: "test1233",
+//         }
+
+//         const response = await request(app)
+//         .post('/customer/login')
+//         .send(body)
+
+//         expect(response.status).toBe(400)
+//         expect(response.body).toBeInstanceOf(Object)
+//         expect(response.body).toHaveProperty('message', expect.any(String))
+//     })
+// })
+
+// describe('POST /customer/login - invalid email', () => {
+//     it('should be return an object with error message', async() => {
+//         const body = {
+//             email: "abhi123@email.com",
+//             password: "12345",
+//         }
+//         const response = await request(app)
+//         .post('/customer/login')
+//         .send(body)
+
+//         expect(response.status).toBe(400)
+//         expect(response.body).toBeInstanceOf(Object)
+//         expect(response.body).toHaveProperty('message', expect.any(String))
+//     })
+// })
--- server/sanbox.js
+++ server/sanbox.js
@@ -8,4 +8,30 @@ dt1 = new Date();
 dt2 = new Date("2022-09-13T14:16:23.396Z");
 console.log(diff_hours(dt1, dt2));
 
+const getHaversineDistance = (firstLocation, secondLocation) => {
+  const earthRadius = 6371; // km 
+
+  const diffLat = (secondLocation.lat-firstLocation.lat) * Math.PI / 180;  
+  const diffLng = (secondLocation.lng-firstLocation.lng) * Math.PI / 180;  
+
+  const arc = Math.cos(
+                  firstLocation.lat * Math.PI / 180) * Math.cos(secondLocation.lat * Math.PI / 180) 
+                  * Math.sin(diffLng/2) * Math.sin(diffLng/2)
+                  + Math.sin(diffLat/2) * Math.sin(diffLat/2);
+  const line = 2 * Math.atan2(Math.sqrt(arc), Math.sqrt(1-arc));
+
+  const distance = earthRadius * line; 
+
+  return distance;
+}
+
+const philly = { lat: 39.9526, lng: -75.1652 }
+const nyc = { lat: 40.7128, lng: -74.0060 }
+const losAngeles = { lat: 34.0522, lng: -118.2437 }
+const jakarta = { lat: 106.81201425438192, lng: -6.1789728520833 }
+
+
+console.log(getHaversineDistance(jakarta, nyc)) //129.61277152662188
+// console.log(getHaversineDistance(philly, losAngeles)) //3843.4534005980404
+
 
