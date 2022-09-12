60ef170b77ce0c71759eb30afcbf18885cb76fdb
Abizrh <abizarah857@gmail.com>
2022-09-12 15:28:21 +0700

    delete and move to new branch

--- server/routers/customerRouter.js
+++ server/routers/customerRouter.js
@@ -6,14 +6,12 @@ const CourtCategoryController = require("../controllers/CourtCategoryController"
 const CourtController = require("../controllers/CourtController");
 const customerRouter = require("express").Router();
 const authentication = require("../middlewares/authentication");
-const CategoryController = require("../controllers/CategoryController");
 
 customerRouter.post("/login", UserController.login); //done
 customerRouter.post("/register", UserController.register); //done
 
 // customerRouter.get("/schedules", SchedulesControllers.getSchedules);
 customerRouter.use(authentication);
-customerRouter.get('/categories', CategoryController.getCategory)
 customerRouter.get("/venues", CourtController.getCourtCustomer); //done, ini daftar GOR yg tersedia
 // customerRouter.get("/venues/:id", CourtController.getDetail);
 customerRouter.get('/courts', CourtCategoryController.getAllCustomer) //ini untuk tampilkan card lapangan di home page
